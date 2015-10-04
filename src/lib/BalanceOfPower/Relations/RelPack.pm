ea50d9418ccd8f9a82dc9a6ac22fba89259094c9
cym0n <cymon@tiscali.it>
2015-10-04 15:39:09 +0200

    Major optimization

--- src/lib/BalanceOfPower/Relations/RelPack.pm
+++ src/lib/BalanceOfPower/Relations/RelPack.pm
@@ -9,6 +9,10 @@ has links => (
     is => 'rw',
     default => sub { [] }
 );
+has links_grid => (
+    is => 'rw',
+    default => sub { {} }
+);
 
 sub all
 {
@@ -21,20 +25,27 @@ sub exists_link
     my $self = shift;
     my $node1 = shift;
     my $node2 = shift;
-    foreach my $r (@{$self->links})
+    if(exists $self->links_grid->{$node1}->{$node2})
     {
-        return $r if($r->is_between($node1, $node2));
+        return $self->links_grid->{$node1}->{$node2} 
     }
-    return undef;
+    else
+    {
+        return undef;
+    }
 }
 
 sub add_link
 {
     my $self = shift;
     my $link = shift;
-    if(! $self->exists_link($link))
+    my $node1 = $link->node1;
+    my $node2 = $link->node2;
+    if(! $self->exists_link($node1, $node2))
     {
         push @{$self->links}, $link;
+        $self->links_grid->{$node1}->{$node2} = $link;
+        $self->links_grid->{$node2}->{$node1} = $link;
         return 1;
     }
     else
@@ -49,18 +60,42 @@ sub update_link
     $self->delete_link($link->node1, $link->node2);
     $self->add_link($link);
 }
+sub delete_references
+{
+    my $self = shift;
+    my $node1 = shift;
+    my $node2 = shift;
+    $self->links_grid->{$node1}->{$node2} = undef;
+    $self->links_grid->{$node2}->{$node1} = undef;
+}
+sub delete_references_for_node
+{
+    my $self = shift;
+    my $node1 = shift;
+    foreach my $k (%{$self->links_grid->{$node1}})
+    {
+        if($k)
+        {
+            $self->links_grid->{$k}->{$node1} = undef;
+        }
+    }
+    $self->links_grid->{$node1} = undef;
+}
 sub delete_link
 {
     my $self = shift;
-    my $n1 = shift;
-    my $n2 = shift;
-    @{$self->links} = grep { ! $_->involve($n1, $n2) } @{$self->links};
+    my $node1 = shift;
+    my $node2 = shift;
+    @{$self->links} = grep { ! $_->involve($node1, $node2) } @{$self->links};
+    $self->delete_references($node1, $node2);
 }
+
 sub delete_link_for_node
 {
     my $self = shift;
     my $n1 = shift;
     @{$self->links} = grep { ! $_->has_node($n1) } @{$self->links};
+    $self->delete_references_for_node($n1);
 }
 sub garbage_collector
 {
@@ -73,6 +108,10 @@ sub garbage_collector
         {
             push @new, $_;
         }
+        else
+        {
+            $self->delete_references($_->node1, $_->node2);
+        }
     }
     @{$self->links} = @new;
 }
@@ -82,11 +121,12 @@ sub links_for_node
     my $node = shift;
     return $self->all() if(! $node);
     my @out = ();
-    foreach my $r (@{$self->links})
+    foreach my $k (keys %{$self->links_grid->{$node}})
     {
-        if($r->has_node($node))
+        if($k)
         {
-            push @out, $r;
+            my $r = $self->links_grid->{$node}->{$k};
+            push @out, $r if($r);
         }
     }
     return @out;
@@ -145,28 +185,32 @@ sub first_link_for_node
 sub link_destinations_for_node
 {
     my $self = shift;
-    my $node = shift;
-    my @nations = ();
-    for(@{$self->links})
+    my $node1 = shift;
+    my @out = ();
+    for(keys %{$self->links_grid->{$node1}})
     {
-        push @nations, $_->destination($node);
+        my $r = $self->links_grid->{$node1}->{$_};
+        if($r)
+        {
+            push @out, $r->destination($node1);
+        }
     }
-    return @nations;
+    return @out;
 }
 
 sub query
 {
     my $self = shift;
     my $query = shift;
-    my $nation = shift;
+    my $node1 = shift;
     my @out = ();
     for(@{$self->links})
     {
         if($query->($_))
         {
-            if($nation)
+            if($node1)
             {
-                if($_->has_node($nation))
+                if($_->has_node($node1))
                 {
                     push @out, $_;
                 }
