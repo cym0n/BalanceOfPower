4e00b2fc39af0aaa9915d36ff02578815cee96ea
cym0n <cymon@tiscali.it>
2016-02-19 01:29:51 +0100

    Web interaction (statics generation)

--- src/lib/BalanceOfPower/World.pm
+++ src/lib/BalanceOfPower/World.pm
@@ -6,6 +6,7 @@ use v5.10;
 use Moo;
 use Data::Dumper;
 use Cwd 'abs_path';
+use File::Path 'make_path';
 
 use BalanceOfPower::Constants ':all';
 use BalanceOfPower::Utils qw(prev_turn next_turn);
@@ -292,6 +293,30 @@ sub elaborate_turn
     $self->pre_decisions_elaborations($t);
     $self->decisions();
     $self->post_decisions_elaborations();
+}
+
+sub generate_whole_turn
+{
+    my $self = shift;
+    my $game = shift;
+    my $turn = shift;
+    my $site_root = shift;
+    $self->pre_decisions_elaborations($turn);
+    $self->build_pre_statics($game, $site_root);
+    $self->decisions();
+    $self->post_decisions_elaborations();
+    $self->build_post_statics($game, $site_root);
+}
+sub generate_web_interactive_turn
+{
+    my $self = shift;
+    my $game = shift;
+    my $site_root = shift;
+    $self->decisions();
+    $self->post_decisions_elaborations();
+    $self->build_post_statics($game, $site_root);
+    $self->pre_decisions_elaborations(next_turn($self->current_year));
+    $self->build_pre_statics($game, $site_root);
 }
 
 #To automatically generate turns
@@ -998,6 +1023,61 @@ sub build_commands
     my $self = shift;
     my $commands = BalanceOfPower::Commands->new( world => $self, log_name => 'bop-commands.log', log_active => $self->log_active );
     return $commands;
+}
+
+
+### Statics
+
+sub build_pre_statics
+{
+    my $self = shift;
+    my $game = shift;
+    my $site_root = shift;
+    my $dest_dir = "$site_root/views/generated/$game/" . $self->current_year();
+    make_path $dest_dir;
+    open(my $hotspots, "> $dest_dir/hotspots.tt");
+    print {$hotspots} $self->print_hotspots('html');  
+    close($hotspots);
+    open(my $allies, "> $dest_dir/alliances.tt");
+    print {$allies} $self->print_allies(undef, 'html');  
+    close($allies);
+    open(my $influences, "> $dest_dir/influences.tt");
+    print {$influences} $self->print_influences(undef, 'html');
+    close($influences);
+    open(my $supports, "> $dest_dir/supports.tt");
+    print {$supports} $self->print_military_supports(undef, 'html');
+    close($supports);
+    open(my $reb_supports, "> $dest_dir/rebel-supports.tt");
+    print {$reb_supports} $self->print_rebel_military_supports(undef, 'html');
+    close($reb_supports);
+    open(my $whistory, "> $dest_dir/war-history.tt");
+    print {$whistory} $self->print_war_history('html');
+    close($whistory);
+}
+
+sub build_post_statics
+{
+    my $self = shift;
+    my $game = shift;
+    my $site_root = shift;
+    my $dest_dir = "$site_root/views/generated/$game/" . next_turn($self->current_year());
+    make_path $dest_dir;
+    open(my $situation, "> $dest_dir/situation.tt");
+    print {$situation} $self->print_turn_statistics($self->current_year(), undef, 'html');  
+    close($situation);
+}
+
+sub build_meta_statics
+{
+    my $self = shift;
+    my $game = shift;
+    my $site_root = shift;
+    my $dest_dir = "$site_root/metadata";
+    make_path $dest_dir;
+    my %data = ( current_year => $self->current_year );
+    open(my $meta, "> $dest_dir/$game.meta");
+    print {$meta} Dumper(\%data);
+    close($meta);
 }
 
 1;
