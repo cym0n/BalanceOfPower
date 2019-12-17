fd9dd1218d1ff3f9b8c7fcbc622e29a8aa5664c8
cymon <cymon@tiscali.it>
2019-12-17 23:40:25 +0100

    docker script better made

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+mkdir -p $HOME/documents/bop-data;
 docker run \
     -d \
     --name balance_of_power \
-    -p 3000:3000 \
-    #-v $HOME/bop-data:/var/lib/mongodb \  # Mount this for persistent data
+    -p=3000:3000 \
+    -v=$HOME/documents/bop-data:/var/lib/mongodb \
     bop
