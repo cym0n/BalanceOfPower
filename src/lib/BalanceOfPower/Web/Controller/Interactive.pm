da047285b24bb93b76defbb4298204e3408e69d5
cymon <cymon@tiscali.it>
2019-12-26 16:14:03 +0100

    add-bet completed

--- src/lib/BalanceOfPower/Web/Controller/Interactive.pm
+++ src/lib/BalanceOfPower/Web/Controller/Interactive.pm
@@ -34,19 +34,23 @@ sub add_player
 sub add_bet
 {
     my $c = shift;
-    my $game = $c->param('game');
-    my $nation = $c->param('nation');
+    my $game = $c->stash('game');
+    my $nation = $c->stash('nation_code');
+    my $current_year = $c->stash('current_year');
     my $lasting = $c->param('lasting');
     my $side = $c->param('side');
     my $value = $c->param('value');
+    my $player = $c->stash('player');
 
-    say "Loading $game";
-    my $world = BalanceOfPower::World->load_mongo($game); 
-    my $nation_obj = $world->get_nation($nation);
-    if(! $nation_obj)
+    if(! $game)
     {
-        die "$nation is not a nation";
+        die "Bad game";
     }
+    if(! $nation)
+    {
+        die "Bad nation";
+    }
+    
     if(! grep {$_ == $lasting} (2, 4, 8) )
     {
         die "Bad value for lasting: $lasting";
@@ -55,17 +59,36 @@ sub add_bet
     {
         die "Bad value for side: $side";
     }
-    my $mongo = MongoDB->connect(); 
-    my $db = $mongo->get_database('bop_' . $game . '_interactions');
-    $db->get_collection('bets')->insert_one({
-                                                game => $game,
-                                                player => $c->stash('player'),
-                                                start_year => $world->current_year,
-                                                value => $value,
-                                                duration => $lasting,
-                                                side => $side
-                                            });
-    $c->redirect_to('/n/' . $game . '/' . $world->current_year . '/' . $nation_obj->code . '/view?alert=bet_ok');
+    my $client = MongoDB->connect(); 
+    my $db = $client->get_database('bop_' . $game . '_interactions');
+    my ($bet) = $db->get_collection('bets')->find({ game => $game, nation => $nation })->all;
+
+    if($bet)
+    {
+        $c->redirect_to('/n/' . $game . '/' . $current_year . '/' . $nation . '/view?alert=bet_already');
+    }
+    else
+    {
+        if($player->{funds} < $value)
+        {
+            $c->redirect_to('/n/' . $game . '/' . $current_year . '/' . $nation . '/view?alert=no_funds');
+        }
+        else
+        {
+            $db->get_collection('bets')->insert_one({
+                                                    game => $game,
+                                                    player => $c->stash('player'),
+                                                    start_year => $current_year,
+                                                    value => $value,
+                                                    duration => $lasting,
+                                                    side => $side,
+                                                    nation => $nation
+                                                });
+            my $new_funds = $player->{funds} - $value;
+            $db->get_collection('players')->update_one({ name => $player->{name}}, { '$set' => { funds => $new_funds }});
+            $c->redirect_to('/n/' . $game . '/' . $current_year . '/' . $nation . '/view?alert=bet_ok');
+        }
+    }
 }
 
 1;
