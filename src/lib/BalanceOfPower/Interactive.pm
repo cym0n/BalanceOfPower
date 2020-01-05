ba503ea3319995bfccf8013976cf9525bd509ba5
cymon <cymon@tiscali.it>
2020-01-05 17:39:55 +0100

    More tools in the script

--- src/lib/BalanceOfPower/Interactive.pm
+++ src/lib/BalanceOfPower/Interactive.pm
@@ -8,6 +8,7 @@ use JSON::Parse 'json_file_to_perl';
 use Cwd 'abs_path';
 use File::Path 'make_path';
 use MongoDB;
+use BalanceOfPower::Utils qw(load_nations_data);
 
 has game => (
     is => 'ro',
@@ -22,6 +23,17 @@ sub _build_event_values {
     $root_path =~ s/Interactive\.pm//;
     my $data_directory = $root_path . "data";
     return json_file_to_perl("$data_directory/events_interactive_values.json");
+}
+has nations_data => (
+    is => 'lazy'
+);
+sub _build_nations_data {
+    my $module_file_path = __FILE__;
+    my $root_path = abs_path($module_file_path);
+    $root_path =~ s/Interactive\.pm//;
+    my $data_directory = $root_path . "data";
+    my %nations_data = load_nations_data("$data_directory/nations-v2.txt");
+    return \%nations_data;
 }
 
 sub elaborate_values
@@ -43,22 +55,23 @@ sub elaborate_values
             my $i = 0;
             for(@{$self->event_values->{$e->{code}}})
             {
+                my $nat_code = $self->nations_data->{$e->{involved}->[$i]}->{code};
                 my $v = $_;
-                if(exists $explanations{$e->{involved}->[$i]})
+                if(exists $explanations{$nat_code})
                 {
-                    push @{$explanations{$e->{involved}->[$i]}}, $e->{code} . ": " . $v;
+                    push @{$explanations{$nat_code}}, $e->{code} . ": " . $v;
                 }
                 else
                 {
-                    $explanations{$e->{involved}->[$i]} = [ $e->{code} . ": " . $v ];
+                    $explanations{$nat_code} = [ $e->{code} . ": " . $v ];
                 }
-                if(exists $values{$e->{involved}->[$i]} )
+                if(exists $values{$nat_code} )
                 {
-                    $values{$e->{involved}->[$i]} += $v
+                    $values{$nat_code} += $v
                 }   
                 else
                 {
-                    $values{$e->{involved}->[$i]} = $v
+                    $values{$nat_code} = $v
                 }
                 $i++;
             }
@@ -74,12 +87,16 @@ sub elaborate_average
     my ($values, undef) = $self->elaborate_values($turn);
     my $count = 0;
     my $sum = 0;
+    my $max = 0;
+    my $min = 1000000;
     for(keys %{$values})
     {
         $count++;
         $sum += $values->{$_};
+        $max = $values->{$_} if $values->{$_} > $max;
+        $min = $values->{$_} if $values->{$_} < $min;
     }
-    return $sum, $count, $sum/$count;
+    return $sum, $count, $sum/$count, $max, $min;
 }
 
 sub calculate_bet_return
@@ -88,11 +105,30 @@ sub calculate_bet_return
     my $turn = shift;
     my $bet = shift;
     my ($values, undef) = $self->elaborate_values($turn);
-    my ($sum, $count, $average) = $self->elaborate_average($turn);
+    my ($sum, $count, $average, $max, $min) = $self->elaborate_average($turn);
     my $nation = $bet->{nation};
-    my $delta = $values->{$nation} - $average;
-    $delta = $delta * (-1) if $bet->{side} eq 'against';
-    my $new_value = $bet->{value} + ( $delta * $bet->{value} );
+    my $add;
+    say "SUM: $sum";
+    say "COUNT: $count";
+    say "AVERAGE: $average";
+    say "MAX: $max";
+    say "MIN: $min";
+    say "Nation is $nation, side is " . $bet->{side} . ", actual value is " . $bet->{value};
+    say "Value is " . $values->{$nation};
+    if($values->{$nation} >= $average)
+    {
+        my $perc = (($values->{$nation} - int($average)) * 100 ) / ($max - int($average));
+        $add = $bet->{value} * ($perc / 100);
+        $add = $add * (-1) if $bet->{side} eq 'against';
+    }
+    else
+    {
+        my $perc = ((int($average) - $values->{$nation}) * 100 ) / (int($average) - $min);
+        $add = $bet->{value} * ($perc / 100);
+        $add = $add * (-1) if $bet->{side} eq 'for';
+    }
+    my $new_value = $bet->{value} + $add; 
+    say "New value ($add): $new_value";
     return sprintf("%.2f", $new_value);
     
 }
